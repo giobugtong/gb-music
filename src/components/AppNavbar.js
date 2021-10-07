db4f8adbf0f6b21811004805a34a00490b57156b
Gio Bugtong <nlbugtong@gmail.com>
2021-10-08 01:51:12 +0800

    Add collapse useState to each Nav Link

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, NavLink, useHistory } from 'react-router-dom';
 import { Navbar, Nav, NavDropdown, OverlayTrigger, Tooltip, Badge, Image } from "react-bootstrap";
 import UserContext from "../UserContext";
@@ -10,6 +10,7 @@ import logoutIcon from "../icons/white-logout.png";
 
 export default function AppNavbar () {
     const { user, unsetUser, fetchUserCart, cartCount } = useContext(UserContext);
+    const [expanded, setExpanded] = useState(false);
 
     const logout = () => {
         unsetUser();
@@ -22,24 +23,24 @@ export default function AppNavbar () {
         user.isAdmin ?
         (
             <>
-                <Nav.Link className="mx-2 d-none d-md-inline icons" as={ NavLink } to="/profile">
+                <Nav.Link onClick={() => setExpanded(false)} className="mx-2 d-none d-md-inline icons" as={ NavLink } to="/profile">
                     <OverlayTrigger key="bottom" placement="bottom" overlay={<Tooltip>
                         My Profile
                     </Tooltip>}>
                     <img width="24" src={profileIcon} alt="Profile icon" />
                     </OverlayTrigger>
                 </Nav.Link>
-                <Nav.Link className="d-md-none" as={ NavLink } to="/profile">My Profile</Nav.Link>
+                <Nav.Link onClick={() => setExpanded(false)} className="d-md-none" as={ NavLink } to="/profile">My Profile</Nav.Link>
                 <OverlayTrigger key="bottom" placement="bottom" overlay={<Tooltip>
                         Logout
                     </Tooltip>}>
                     
-                <Nav.Link className="mx-2 d-none d-md-inline icons" onClick={logout} to="/logout">
+                <Nav.Link onClick={() => setExpanded(false)} className="mx-2 d-none d-md-inline icons" onClick={logout} to="/logout">
                 <img width="25" alt="Logout icon" src={logoutIcon} />
                 </Nav.Link>
                 </OverlayTrigger>
 
-                <Nav.Link className="d-md-none" onClick={logout} to="/logout">Logout</Nav.Link>
+                <Nav.Link onClick={() => setExpanded(false)} className="d-md-none" onClick={logout} to="/logout">Logout</Nav.Link>
             </>
         ) :
         (  
@@ -48,21 +49,21 @@ export default function AppNavbar () {
                     <Tooltip>
                         My Cart
                     </Tooltip>}>
-                <Nav.Link className="mx-1 d-none d-md-block icons" as={ NavLink } to="/my-cart">
+                <Nav.Link onClick={() => setExpanded(false)} className="mx-1 d-none d-md-block icons" as={ NavLink } to="/my-cart">
                     <img width="24" alt="Shopping cart icon" src={shoppingCart}/>
                     <span style={{display: cartCount > 0 ? "inline-block" : "none"}}>
                     <Badge style={{marginLeft: "-3px"}} variant="light">{cartCount}</Badge>    
                     </span>
                 </Nav.Link>
                     </OverlayTrigger>
-                <Nav.Link className="d-md-none" as={ NavLink } to="/my-cart">
+                <Nav.Link onClick={() => setExpanded(false)} className="d-md-none" as={ NavLink } to="/my-cart">
                 My Cart
                 <span style={{display: cartCount > 0 ? "inline-block" : "none"}}>
                     <Badge style={{marginLeft: "5px"}} variant="light">{cartCount}</Badge>    
                     </span>
                 </Nav.Link>
                 
-                <Nav.Link className="mx-2 d-none d-md-inline icons" as={ NavLink } to="/profile">
+                <Nav.Link onClick={() => setExpanded(false)} className="mx-2 d-none d-md-inline icons" as={ NavLink } to="/profile">
                     <OverlayTrigger key="bottom" placement="bottom" overlay={<Tooltip>
                         My Profile
                     </Tooltip>}>
@@ -70,32 +71,32 @@ export default function AppNavbar () {
                     </OverlayTrigger>
                 </Nav.Link>
 
-                <Nav.Link className="d-md-none" as={ NavLink } to="/profile">My Profile</Nav.Link>
+                <Nav.Link onClick={() => setExpanded(false)} className="d-md-none" as={ NavLink } to="/profile">My Profile</Nav.Link>
                 
                 <OverlayTrigger key="bottom" placement="bottom" overlay={<Tooltip>
                         Logout
                     </Tooltip>}>
                     
-                <Nav.Link className="mx-2 d-none d-md-inline icons" onClick={logout} to="/logout">
+                <Nav.Link onClick={() => setExpanded(false)} className="mx-2 d-none d-md-inline icons" onClick={logout} to="/logout">
                 <img width="25" alt="Logout icon" src={logoutIcon} />
                 </Nav.Link>
                 </OverlayTrigger>
 
                 
                 
-                <Nav.Link className="d-md-none" onClick={logout} to="/logout">Logout</Nav.Link>
+                <Nav.Link onClick={() => setExpanded(false)} className="d-md-none" onClick={logout} to="/logout">Logout</Nav.Link>
             </>
         )    
     ) :
     (
          <>
-            <Nav.Link as={ NavLink } to="/register">Register</Nav.Link>
-            <Nav.Link as={ NavLink } to="/login">Login</Nav.Link>
+            <Nav.Link onClick={() => setExpanded(false)} as={ NavLink } to="/register">Register</Nav.Link>
+            <Nav.Link onClick={() => setExpanded(false)} as={ NavLink } to="/login">Login</Nav.Link>
         </>
     )
 
     return(
-        <Navbar id="navbar" variant="dark" expand="md">
+        <Navbar id="navbar" variant="dark" expand="md" expanded={expanded}>
             <Navbar.Brand as={ Link } to="/">
             <Image
                 src={logo}
@@ -105,7 +106,7 @@ export default function AppNavbar () {
                 alt="g.b. music logo"
             />
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => expanded ? setExpanded(false) : setExpanded(true)}/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <NavDropdown href="/products" title="Products"  id="basic-nav-dropdown">
